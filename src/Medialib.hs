3a57a162453f78d93f639032351e670ff688fb11
Oleg Belozeorov <upwawet@gmail.com>
2010-06-18 19:02:41 +0600

    implement mediainfo retrieval and change handling

--- src/Medialib.hs
+++ src/Medialib.hs
@@ -21,43 +21,89 @@ module Medialib
   ( Stamp
   , MediaInfo
   , initMedialib
+  , onMediaInfo
   ) where
+
+import Control.Concurrent.MVar
+import Control.Monad
+import Control.Monad.Trans
 
 import Data.Map (Map)
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
+import Data.Maybe
 
 import XMMS2.Client
---import XMMS2.Client.Bindings (propdictToDict)
+import XMMS2.Client.Bindings (propdictToDict)
 
+import Utils
 import Env
+import XMMS
+import Handler
 
 
 type Stamp = Int
 type MediaInfo = Map String Property
 
+
 data CacheEntry
   = CEReady Stamp MediaInfo
   | CERetrieving
 
-type Cache = IntMap CacheEntry
+data Cache
+  = Cache { cEntries   :: IntMap CacheEntry
+          , cNextStamp :: Stamp
+          }
+
+emptyCache =
+  Cache { cEntries   = IntMap.empty
+        , cNextStamp = 1
+        }
+
 
 data MLib
-  = MLib { mCache     :: Cache
-         , mNextStamp :: Stamp
+  = MLib { mCache       :: MVar Cache
+         , mOnMediaInfo :: HandlerMVar (Stamp, MediaInfo)
          }
 
-withCache f = f $ mCache getEnv
+cache = mCache getEnv
 
+
+onMediaInfo = onHandler $ mOnMediaInfo getEnv
 
 initMedialib = do
   env <- initEnv
   let ?env = env
 
+  onConnected . add . ever . const $
+    broadcastMedialibEntryChanged xmms >>* do
+      id <- result
+      let id' = fromIntegral id
+      liftIO $ do
+        withMVar cache $ \cache ->
+          when (isJust . IntMap.lookup id' $ cEntries cache) $
+            medialibGetInfo xmms id >>* handleInfo id'
+      return True
+
   return ?env
 
 
 initEnv = do
+  cache       <- newMVar emptyCache
+  onMediaInfo <- makeHandlerMVar
   return $ augmentEnv
-    MLib { mCache     = IntMap.empty
-         , mNextStamp = 0 }
+    MLib { mCache       = cache
+         , mOnMediaInfo = onMediaInfo }
+
+handleInfo id = do
+  rawv <- resultRawValue
+  liftIO $ do
+    info  <- valueGet =<< propdictToDict rawv []
+    stamp <- modifyMVar cache $ \cache ->
+      let stamp   = cNextStamp cache
+          entries = cEntries cache
+          entry   = CEReady stamp info in
+      return (Cache { cEntries   = IntMap.insert id entry entries
+                    , cNextStamp = succ stamp }, stamp)
+    onMediaInfo $ invoke (stamp, info)
+  return False
