0787127197a1a83561129e52458bd6a034119da1
Oleg Belozeorov <upwawet@gmail.com>
2010-09-28 19:01:10 +0600

    attempt to describe drag dests declaratively

--- src/Playlist/DnD.hs
+++ src/Playlist/DnD.hs
@@ -17,14 +17,18 @@
 --  General Public License for more details.
 --
 
+{-# LANGUAGE TypeSynonymInstances, ExistentialQuantification #-}
+
 module Playlist.DnD
   ( setupDnD
   ) where
 
+import Control.Applicative
 import Control.Monad
 import Control.Monad.Trans
 
 import Data.IORef
+import qualified Data.IntMap as IntMap
 
 import Graphics.UI.Gtk
 
@@ -56,55 +60,89 @@ setupDnD = do
     selectionDataSet selectionTypeInteger $ map head rows
     return ()
 
-  targetList <- targetListNew
-  targetListAdd targetList xmms2PosListTarget [TargetSameWidget] 0
-  targetListAdd targetList xmms2MlibIdTarget [TargetSameApp] 1
-  targetListAddUriTargets targetList 2
-
-  dragDestSet view [DestDefaultMotion, DestDefaultHighlight]
+  setupDest view
+    [DestDefaultMotion, DestDefaultHighlight]
     [ActionMove, ActionCopy]
-  dragDestSetTargetList view targetList
+    [ xmms2PosListTarget :>: \(_, y) -> do
+         rows <- selectionDataGet selectionTypeInteger
+         liftIO $ withJust rows $ \rows -> do
+           base <- getTargetRow store view y True
+           moveTracks $ reorder base rows
+         return (True, True)
+    , xmms2MlibIdTarget :>: \(_, y) -> do
+         ids <- selectionDataGet selectionTypeInteger
+         liftIO $ withJust ids $ \ids -> do
+           name <- getPlaylistName
+           base <- getTargetRow store view y False
+           zipWithM_ (playlistInsertId xmms name) [base .. ] ids
+         return (True, False)
+    , URITargets :>: \(_, y) -> do
+         uris <- selectionDataGetURIs
+         liftIO $ withJust uris $ \uris -> do
+           base <- getTargetRow store view y False
+           insertURIs uris $ Just base
+         return (True, False)
+    ]
+
+  view `on` dragDataDelete $ \_ ->
+    treeSelectionUnselectAll sel
+
+
+data CommonTargets
+  = URITargets
+
+class TargetClass t where
+  addToTargetList :: TargetList -> InfoId -> t -> IO ()
+
+instance TargetClass TargetTag where
+  addToTargetList tl id tg = targetListAdd tl tg [] id
+
+instance TargetClass CommonTargets where
+  addToTargetList tl id tg =
+    case tg of
+      URITargets -> targetListAddUriTargets tl id
+
+data Dest
+  = forall t. TargetClass t => t :|: Dest
+  | forall t. TargetClass t => t :>: (Point -> SelectionDataM (Bool, Bool))
+
+infixr 9 :|:, :>:
+
+
+setupDest widget defs acts dests = do
+  tl <- targetListNew
+  hm <- IntMap.fromList <$> zipWithM (setupDest' tl) [0 .. ] dests
+
+  dragDestSet widget defs acts
+  dragDestSetTargetList widget tl
 
   dropRef <- newIORef False
 
-  view `on` dragDrop $ \ctxt _ tstamp -> do
-    maybeTarget <- dragDestFindTarget view ctxt (Just targetList)
+  widget `on` dragDrop $ \ctxt _ tstamp -> do
+    maybeTarget <- dragDestFindTarget widget ctxt (Just tl)
     case maybeTarget of
       Just target -> do
         writeIORef dropRef True
-        dragGetData view ctxt target tstamp
+        dragGetData widget ctxt target tstamp
         return True
       Nothing ->
         return False
 
-  view `on` dragDataReceived $ \ctxt (_, y) infoId tstamp -> do
+  widget `on` dragDataReceived $ \ctxt pos infoId tstamp -> do
     drop <- liftIO $ readIORef dropRef
     when drop $ do
       liftIO $ writeIORef dropRef False
-      case infoId of
-        0 -> do
-          rows <- selectionDataGet selectionTypeInteger
-          liftIO $ do
-            withJust rows $ \rows -> do
-              base <- getTargetRow store view y True
-              moveTracks $ reorder base rows
-            dragFinish ctxt True True tstamp
-        1 -> do
-          ids <- selectionDataGet selectionTypeInteger
-          liftIO $ do
-            withJust ids $ \ids -> do
-              name <- getPlaylistName
-              base <- getTargetRow store view y False
-              zipWithM_ (playlistInsertId xmms name) [base .. ] ids
-            dragFinish ctxt True False tstamp
-        2 -> do
-          uris <- selectionDataGetURIs
-          liftIO $ do
-            withJust uris $ \uris -> do
-              base <- getTargetRow store view y False
-              insertURIs uris $ Just base
-            dragFinish ctxt True False tstamp
-        _ -> liftIO $ dragFinish ctxt False False tstamp
+      (ok, del) <- case IntMap.lookup (fromIntegral infoId) hm of
+        Just handler -> handler pos
+        Nothing      -> return (False, False)
+      liftIO $ dragFinish ctxt ok del tstamp
 
-  view `on` dragDataDelete $ \_ ->
-    treeSelectionUnselectAll sel
+  return ()
+
+setupDest' tl id (t :|: rest) = do
+  addToTargetList tl id t
+  setupDest' tl id rest
+setupDest' tl id (t :>: handler) = do
+  addToTargetList tl id t
+  return (fromIntegral id, handler)
+
