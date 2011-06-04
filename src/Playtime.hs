fac17d25dbc31012b8ba4839843bb3e1f2cf8644
Oleg Belozeorov <upwawet@gmail.com>
2011-06-04 17:31:50 +0600

    start Playtime rewrite

--- src/Playtime.hs
+++ src/Playtime.hs
@@ -47,142 +47,67 @@ import Utils
 import Playback
 
 
-data State
-  = State { sCurrentId :: Maybe MediaId }
-
 data Playtime
-  = Playtime { pState :: MVar State
-             , pAdj   :: Adjustment
-             , pCId   :: ConnectId Adjustment }
+  = Playtime { pAdj :: Adjustment }
 
-state = pState context
-cId   = pCId context
-adj   = pAdj context
+adj = pAdj context
 
-getCurrentId    = withMVar state $ return . sCurrentId
-setCurrentId id = modifyMVar_ state $ \s -> return  s { sCurrentId = id }
+data Msg
+  = PT Int
+  | CI (Maybe MediaId)
+  | MI (MediaId, Stamp, MediaInfo)
 
 initPlaytime = do
-  updateRef <- initUpdate
-  let ?updateRef = updateRef
-
   context <- initContext
   let ?context = context
 
+  ciV <- atomically $ newTVar Nothing
+  ciW <- atomically $ newTWatch ciV Nothing
+  ptV <- atomically $ newTVar 0
+  ptW <- atomically $ newEmptyTWatch ptV
+  miC <- atomically $ dupTChan mediaInfoChan
   onServerConnectionAdd . ever $ \conn ->
     if conn
     then do
-      playbackCurrentId xmms >>* handleCurrentId
-      broadcastPlaybackCurrentId xmms >>* (handleCurrentId >> persist)
-      playbackPlaytime xmms >>* handlePlaytime 0
-      signalPlaybackPlaytime xmms >>* (handlePlaytime 1000 >> persist)
+      playbackCurrentId xmms >>* handleCurrentId ciV
+      broadcastPlaybackCurrentId xmms >>* (handleCurrentId ciV >> persist)
+      playbackPlaytime xmms >>* handlePlaytime ptV
+      signalPlaybackPlaytime xmms >>* (handlePlaytime ptV >> persist)
     else do
-      resetState
-      setValue 0
-      resetUpdate (+ 1)
+      return ()
 
-  (atomically $ dupTChan mediaInfoChan) >>= forkIO . handleInfo
-  onPlaybackStatusAdd . ever $ \status ->
-    when (fromMaybe StatusStop status == StatusStop) $
-      setValue 0
+  forkIO $ forever $ do
+    msg <- atomically $ msum [CI <$> watch ciW, PT <$> watch ptW, MI <$> readTChan miC]
+    case msg of
+      PT time -> adjustmentSetValue adj $ fromIntegral time
+      CI mmid -> do
+        adjustmentSetValue adj 0
+        adjustmentSetUpper adj 0
+        withJust mmid requestInfo
+      MI (mi, _, info) -> do
+        ci <- readTVarIO ciV
+        when (ci == Just mi) $
+          case Map.lookup "duration" info of
+            Just (PropInt32 d) -> adjustmentSetUpper adj $ fromIntegral d
+            _                  -> return ()
 
   return ?context
 
+handlePlaytime ptV = do
+  pt <- catchResult 0 fromIntegral
+  liftIO $ atomically $ writeTVar ptV pt
+
+handleCurrentId ciV = do
+  ci <- catchResult Nothing Just
+  liftIO $ atomically $ writeTVar ciV ci
+
 
 initContext = do
-  state <- newMVar makeState
   adj <- adjustmentNew 0 0 bigNum 5000 5000 0
-  cId <- adj `onValueChanged` do
-    pos <- adjustmentGetValue adj
-    seek pos
-
   return $ augmentContext
-    Playtime { pState = state
-             , pAdj   = adj
-             , pCId   = cId }
-
-makeState =
-  State { sCurrentId = Nothing }
-
-resetState =
-  modifyMVar_ state $ const $ return makeState
-
-handleCurrentId = do
-  cid <- result
-  liftIO $ do
-    setValue 0
-    setUpper bigNum
-    setCurrentId $
-      if cid == 0
-      then Nothing
-      else Just cid
-    requestInfo cid
-
-handleInfo chan = forever $ do
-  (id, _, info) <- atomically $ readTChan chan
-  cid <- getCurrentId
-  when (cid == Just id) $
-    case Map.lookup "duration" info of
-      Just (PropInt32 d) -> setUpper $ fromIntegral d
-      _                  -> return ()
+    Playtime { pAdj = adj }
 
 bigNum = 10000000000.0
-
-seek pos = do
-  eid <- disableUpdate
-  playbackSeekMs xmms (round pos) SeekSet >>*
-    liftIO (scheduleEnableUpdate eid)
-  return ()
-
-data Update
-  = Update { uEnabled   :: Bool
-           , uEnablerId :: Maybe HandlerId
-           , uStamp     :: Word32 }
-
-initUpdate =
-  newIORef Update { uEnabled   = True
-                  , uEnablerId = Nothing
-                  , uStamp     = 0 }
-
-resetUpdate f =
-  modifyIORef ?updateRef $ \up ->
-    up { uEnabled   = True
-       , uEnablerId = Nothing
-       , uStamp     = f $ uStamp up }
-
-updateEnabled = uEnabled <$> readIORef ?updateRef
-
-disableUpdate = do
-  up <- readIORef ?updateRef
-  maybe (return ()) timeoutRemove $ uEnablerId up
-  let stamp = uStamp up + 1
-  writeIORef ?updateRef Update { uEnabled   = False
-                               , uEnablerId = Nothing
-                               , uStamp     = stamp }
-  return stamp
-
-scheduleEnableUpdate stamp = do
-  eid <- flip timeoutAdd 500 $ do
-    stamp' <- uStamp <$> readIORef ?updateRef
-    when (stamp == stamp') $ resetUpdate id
-    return False
-  modifyIORef ?updateRef $ \up -> up { uEnablerId = Just eid }
-
-getValue = adjustmentGetValue adj
-setValue = withoutSeek . adjustmentSetValue adj
-setUpper = withoutSeek . adjustmentSetUpper adj
-
-withoutSeek =
-  bracket_ (signalBlock cId) (signalUnblock cId)
-
-handlePlaytime diff = do
-  newPt <- catchResult 0 fromIntegral
-  liftIO $ do
-    en <- updateEnabled
-    ps <- getPlaybackStatus
-    when (en && fromMaybe StatusStop ps /= StatusStop) $ do
-      oldPt <- getValue
-      when (abs (newPt - oldPt) >= diff) $ setValue newPt
 
 makeSeekControl = do
   view <- hScaleNew adj
