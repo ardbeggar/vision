e798d7c8956bdf5165bc1ba3c3e9c4686dd0b3a1
Oleg Belozeorov <upwawet@gmail.com>
2011-06-09 17:12:30 +0600

    prototype STM-based XMMS connection state sync notification

--- src/XMMS.hs
+++ src/XMMS.hs
@@ -23,10 +23,19 @@ module XMMS
   , connected
   , onServerConnection
   , onServerConnectionAdd
+  , SN ()
+  , mkSN
+  , activateSN
+  , waitSN
+  , doneSN
   ) where
 
 import Prelude hiding (init)
+import Control.Applicative
+import Control.Concurrent
 import Control.Concurrent.STM
+
+import Data.Unique
 
 import Graphics.UI.Gtk hiding (add)
 
@@ -43,6 +52,9 @@ data XMMS
   = XMMS { xXMMS               :: Connection
          , xOnServerConnection :: HandlerMVar Bool
          , xConnected          :: TVar Bool
+         , xConnectionId       :: TVar Unique
+         , xNWatchers          :: TVar Int
+         , xNWaiting           :: TVar Int
          }
 
 xmms               = xXMMS context
@@ -63,10 +75,17 @@ initContext = do
   xmms               <- init "Vision"
   onServerConnection <- makeHandlerMVar
   connected          <- atomically $ newTVar False
+  id                 <- newUnique
+  connectionId       <- atomically $ newTVar id
+  nWatchers          <- atomically $ newTVar 0
+  nWaiting           <- atomically $ newTVar 0
   return $ augmentContext
     XMMS { xXMMS               = xmms
          , xOnServerConnection = onServerConnection
          , xConnected          = connected
+         , xConnectionId       = connectionId
+         , xNWatchers          = nWatchers
+         , xNWaiting           = nWaiting
          }
 
 scheduleTryConnect = timeoutAdd tryConnect
@@ -77,7 +96,7 @@ tryConnect = do
     then do
     disconnectCallbackSet xmms disconnectCallback
     mainLoopGMainInit xmms
-    atomically $ writeTVar (xConnected context) True
+    setConnected True
     onServerConnection $ invoke True
     return False
     else do
@@ -85,7 +104,7 @@ tryConnect = do
     return False
 
 disconnectCallback = do
-  atomically $ writeTVar (xConnected context) False
+  setConnected False
   onServerConnection $ invoke False
   scheduleTryConnect 1000
   return ()
@@ -94,3 +113,53 @@ onServerConnectionAdd f = do
   id <- onServerConnection . add $ f
   f =<< connected
   return id
+
+setConnected conn = do
+  putStrLn $ "Notify: " ++ show conn
+  id <- newUnique
+  atomically $ do
+    writeTVar (xConnected context) conn
+    writeTVar (xConnectionId context) id
+    writeTVar (xNWaiting context) =<< readTVar (xNWatchers context)
+  yield
+  atomically $ do
+    nw <- readTVar $ xNWaiting context
+    if nw == 0
+      then return ()
+      else retry
+  putStrLn "done"
+
+
+data SN a =
+  SN { snValue   :: TVar a
+     , snValueId :: TVar Unique
+     , snOurId   :: TVar (Maybe Unique)
+     , snNWatch  :: TVar Int
+     , snNWait   :: TVar Int
+     }
+
+mkSN = do
+  ourId <- newTVar Nothing
+  return SN { snValue   = xConnected context
+            , snValueId = xConnectionId context
+            , snOurId   = ourId
+            , snNWatch  = xNWatchers context
+            , snNWait   = xNWaiting context
+            }
+
+activateSN sn = do
+  nw <- readTVar (snNWatch sn)
+  writeTVar (snNWatch sn) (nw + 1)
+
+waitSN sn = do
+  oid <- readTVar (snOurId sn)
+  vid <- Just <$> readTVar (snValueId sn)
+  if oid == vid
+    then retry
+    else do
+    writeTVar (snOurId sn) vid
+    readTVar (snValue sn)
+
+doneSN sn = do
+  nw <- readTVar (snNWait sn)
+  writeTVar (snNWait sn) (nw - 1)
