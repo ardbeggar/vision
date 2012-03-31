abc6ff850bcfa9bc7452addae7eeafaf2222cd91
Oleg Belozeorov <upwawet@gmail.com>
2012-03-31 14:42:39 +0600

    add module: Library.Filter

---
+++ src/Library/Filter.hs
@@ -0,0 +1,30 @@
+-- -*-haskell-*-
+--  Vision (for the Voice): an XMMS2 client.
+--
+--  Author:  Oleg Belozeorov
+--  Created: 31 Mar. 2012
+--
+--  Copyright (C) 2012 Oleg Belozeorov
+--
+--  This program is free software; you can redistribute it and/or
+--  modify it under the terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 3 of
+--  the License, or (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+--  General Public License for more details.
+--
+
+module Library.Filter
+  (
+  ) where
+
+import Data.Set (Set)
+
+data Props = Props deriving (Show)
+data Colls = Colls deriving (Show)
+
+newtype SetOf a = SetOf { unSetOf :: Set String } deriving (Show)
+
